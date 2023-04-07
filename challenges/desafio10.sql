9b6206f6d9f58ab85d0c1d62746db965cdcc1d00
Luis <l-serrato@live.com>
2023-04-06 23:47:49 -0300

    req 10

--- challenges/desafio10.sql
+++ challenges/desafio10.sql
@@ -1,13 +1,24 @@
--- Descomente e altere as linhas abaixo:
+USE SpotifyClone;
 
--- CREATE TABLE SpotifyClone.tabela(
---     coluna1 tipo restricoes,
---     coluna2 tipo restricoes,
---     colunaN tipo restricoes,
--- ) engine = InnoDB;
+CREATE TABLE fav_relacao 
+(
+    pessoa_id INT,
+    musica_id INT,
+    CONSTRAINT PRIMARY KEY(pessoa_id, musica_id),
+    FOREIGN KEY (pessoa_id) REFERENCES usuario_tabela (pessoa_usuaria_id),
+    FOREIGN KEY (musica_id) REFERENCES musica_tabela (cancao_id)
+) engine = InnoDB;
 
--- INSERT INTO SpotifyClone.tabela (coluna1, coluna2)
--- VALUES
---   ('exemplo de dados 1', 'exemplo de dados A'),
---   ('exemplo de dados 2', 'exemplo de dados B'),
---   ('exemplo de dados 3', 'exemplo de dados C');
+INSERT INTO fav_relacao (pessoa_id, musica_id) VALUES (1, 1);
+INSERT INTO fav_relacao (pessoa_id, musica_id) VALUES (1, 2);
+INSERT INTO fav_relacao (pessoa_id, musica_id) VALUES (1, 3);
+INSERT INTO fav_relacao (pessoa_id, musica_id) VALUES (2, 4);
+INSERT INTO fav_relacao (pessoa_id, musica_id) VALUES (3, 5);
+INSERT INTO fav_relacao (pessoa_id, musica_id) VALUES (3, 1);
+INSERT INTO fav_relacao (pessoa_id, musica_id) VALUES (4, 6);
+INSERT INTO fav_relacao (pessoa_id, musica_id) VALUES (4, 4);
+INSERT INTO fav_relacao (pessoa_id, musica_id) VALUES (5, 3);
+INSERT INTO fav_relacao (pessoa_id, musica_id) VALUES (5, 7);
+INSERT INTO fav_relacao (pessoa_id, musica_id) VALUES (6, 4);
+INSERT INTO fav_relacao (pessoa_id, musica_id) VALUES (7, 6);
+INSERT INTO fav_relacao (pessoa_id, musica_id) VALUES (8, 1);
